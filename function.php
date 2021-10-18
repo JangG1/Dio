15cced65ad609c78b03374a076cf120e15a81b27
Dio <91955876+JangG1@users.noreply.github.com>
2021-10-18 23:21:38 +0900

    Add files via upload

--- function.php
+++ function.php
@@ -5,7 +5,7 @@
     <title>Function</title>
   </head>
   <body>
-    <h1>function </h1>
+    <h1>function</h1>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     <?php
      $str = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,
