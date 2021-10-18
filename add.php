07efb1e3852c4fb277e3bec23e90332865f6e4cc
Dio <91955876+JangG1@users.noreply.github.com>
2021-10-18 23:20:22 +0900

    Add files via upload

---
+++ add.php
@@ -0,0 +1,7 @@
+<?php
+file_put_contents('Modification/'.$_POST['menuname'], $_POST['description']);
+/*
+echo "<p>title : ".$_GET['title']."</p>";
+echo "<p>description : ".$_GET['description']."</p>";
+*/
+?>
---
+++ add2.php
@@ -0,0 +1,7 @@
+<?php
+file_put_contents('Menulist/'.$_POST['category'], $_POST['description']);
+/*
+echo "<p>title : ".$_GET['title']."</p>";
+echo "<p>description : ".$_GET['description']."</p>";
+*/
+?>
